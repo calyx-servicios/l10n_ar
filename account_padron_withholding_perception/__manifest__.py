bf04ae34c167fa3587bf80ea6bcb8e5b8997248d
JaviZamoraDev <javier.zamora.dev@gmail.co>
2023-07-06 11:36:44 -0300

    [ODOO-6400][FIX][account_padron_withholdin_perception] - Fix translations

--- account_padron_withholding_perception/__manifest__.py
+++ account_padron_withholding_perception/__manifest__.py
@@ -10,7 +10,7 @@
     "website": "http://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Account",
-    "version": "15.0.1.4.0",
+    "version": "15.0.1.4.1",
     "installable": True,
     "application": False,
     "depends": [
