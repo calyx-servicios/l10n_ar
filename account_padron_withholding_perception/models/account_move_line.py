928d608a774c2b78761000c5b41d2d7848df3fd0
JaviZamoraDev <javier.zamora.dev@gmail.co>
2023-07-21 09:43:34 -0300

    [ODOO-6400][ODOO-6952] - Fix user error, fix date, fix perception, fix retention, fix tax.

--- account_padron_withholding_perception/models/account_move_line.py
+++ account_padron_withholding_perception/models/account_move_line.py
@@ -7,19 +7,21 @@ class AccountMoveLine(models.Model):
     ret_perc_type_ids = fields.Many2many('account.padron.retention.perception.type',
                                 'rel_invoice_line_padron_type',
                                 'invoice_line_id',
-                                'padron_type_id', string="Padron Type", readonly=0)
+                                'padron_type_id', string="Padron Type", store=True)
+
+    @api.model
+    def create(self, vals):
+        if 'move_id' in vals:
+            move = self.env['account.move'].browse(vals['move_id'])
+            if move.partner_id and move.move_type in ('in_invoice', 'in_refund'):  # Supplier Invoice
+                padron_types = move.partner_id.line_padron_type_ids
+                if padron_types:
+                    vals['ret_perc_type_ids'] = [(6, 0, padron_types.ids)]
+        return super(AccountMoveLine, self).create(vals)
 
     @api.onchange('product_id')
     def onchange_product_id(self):
-        if self.move_id.partner_id and self.move_id.move_type in ('in_invoice','in_refund'): # Factura proveedor
-            list_add =  []
-            for line_obj in self.move_id.partner_id.line_padron_type_ids:
-                list_add.append(line_obj.id)
-            self.ret_perc_type_ids = list_add
-
-    @api.onchange('product_id', 'move_id.invoice_date')
-    def onchange_product_id(self):
-        if self.move_id.partner_id and self.move_id.move_type in ('out_invoice', 'out_refund'):
+        if self.move_id.partner_id and self.move_id.move_type in ('out_invoice', 'out_refund'): # Customer Invoice
             if self.move_id.partner_id.line_padron_type_ids:
                 domain = [
                     ('partner_id', '=', self.move_id.partner_id.id),
@@ -30,5 +32,6 @@ class AccountMoveLine(models.Model):
                 if arba_line and self.product_id:
                     tax_id = arba_line.padron_line_id.import_padron_id.padron_type_id.account_tax_perception_id.id
                     self.tax_ids = [(4, tax_id)]
+
 
 
--- account_padron_withholding_perception/models/res_partner.py
+++ account_padron_withholding_perception/models/res_partner.py
@@ -4,15 +4,15 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    line_padron_type_ids = fields.Many2many('account.padron.retention.perception.type', 
-                                    'res_partner_padron_type_rel',  'padron_type_id', 'partner_id', 
+    line_padron_type_ids = fields.Many2many('account.padron.retention.perception.type',
+                                    'res_partner_padron_type_rel',  'padron_type_id', 'partner_id',
                                     string="Rel Partner Patterns", copy=False, readonly=False)
 
 
     @api.model
     def create(self, values):
         record = super(ResPartner, self).create(values)
-        if record.l10n_ar_afip_responsibility_type_id.code not in ('5', '7', '8', '9') and self.env['ir.config_parameter'].get_param('account_padron_withholding_perception.check_census_on_create'):
+        if (record.l10n_ar_afip_responsibility_type_id.code not in ('5', '7', '8', '9')) and self.env['ir.config_parameter'].get_param('account_padron_withholding_perception.check_census_on_create') and record.vat:
             padrones = self.env['account.padron.retention.perception.type'].search([]).ids
             record.write({'line_padron_type_ids': [(6, 0, padrones)]})
         return record
@@ -30,7 +30,7 @@ class ResPartner(models.Model):
 
     def import_padron_server_partner(self, context={}):
         partner_dic={} #padron_type_id
-        for partner_obj in self:  
+        for partner_obj in self:
             partner_dic[partner_obj.vat] = partner_obj
             context['partner_dic'] = partner_dic
             for padron_obj in partner_obj.line_padron_type_ids:
@@ -40,4 +40,17 @@ class ResPartner(models.Model):
                                                                 ])
                 for import_obj in result_ids:
                     import_obj.import_padron_server(context=context)
+
+    def get_current_alicuota(self, padron_type, date_invoice):
+        current_alicuota = None
+
+        for alicuota in self.arba_alicuot_ids:
+            if alicuota.tag_id.id == padron_type.account_tag_perception_id.id:
+                if alicuota.from_date <= date_invoice <= alicuota.to_date:
+                    current_alicuota = alicuota
+                    break
+
+        return current_alicuota
+
+
 
