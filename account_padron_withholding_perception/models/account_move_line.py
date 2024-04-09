de750c1febdac3bdcb10a7c1386d6878ab99ec2d
Leandro090685 <Leandro090685@github.com>
2024-04-09 10:26:39 -0300

    FIX-BPO-ARBA-LINE

--- account_padron_withholding_perception/models/account_move_line.py
+++ account_padron_withholding_perception/models/account_move_line.py
@@ -9,13 +9,14 @@ class AccountMoveLine(models.Model):
     def onchange_product_id_perception(self):
         if self.move_id.partner_id and self.move_id.move_type in ('out_invoice', 'out_refund'):
             if self.move_id.partner_id.line_padron_type_ids and self.product_id:
-                arba_line = self.move_id.partner_id.arba_alicuot_ids.filtered(
+                arba_lines = self.move_id.partner_id.arba_alicuot_ids.filtered(
                     lambda x: x.to_date >= self.move_id.invoice_date
                     and x.from_date <= self.move_id.invoice_date
                     and x.company_id.id == self.move_id.company_id.id
                 )
-                if arba_line:
-                    padrons_type = arba_line.padron_line_id.import_padron_id.padron_type_id
-                    tax_id = padrons_type.account_tax_perception_id
-                    if tax_id and tax_id not in self.tax_ids:
-                        self.tax_ids = [(4, tax_id.id)]
+                if arba_lines:
+                    for arba_line in arba_lines:
+                        padrons_type = arba_line.padron_line_id.import_padron_id.padron_type_id
+                        tax_id = padrons_type.account_tax_perception_id
+                        if tax_id and tax_id not in self.tax_ids:
+                            self.tax_ids = [(4, tax_id.id)]
